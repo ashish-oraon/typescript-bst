7081e982ad34521b46fdcf5f113a12e13179f24f
ashish-oraon <ashish.becs@hotmail.com>
2022-07-13 11:27:17 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-vknz4o
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-vknz4o)
---
+++ index.ts
@@ -0,0 +1,140 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+export class BinarySearchTreeNode<T> {
+  data: T;
+  leftNode?: BinarySearchTreeNode<T>;
+  rightNode?: BinarySearchTreeNode<T>;
+
+  constructor(data: T) {
+    this.data = data;
+  }
+}
+
+export class BinarySearchTree<T> {
+  root?: BinarySearchTreeNode<T>;
+  comparator: (a: T, b: T) => number;
+
+  constructor(comparator: (a: T, b: T) => number) {
+    this.comparator = comparator;
+  }
+
+  insert(data: T): BinarySearchTreeNode<T> | undefined {
+    if (!this.root) {
+      this.root = new BinarySearchTreeNode(data);
+      return this.root;
+    }
+
+    let current = this.root;
+
+    while (true) {
+      if (this.comparator(data, current.data) === 1) {
+        if (current.rightNode) {
+          current = current.rightNode;
+        } else {
+          current.rightNode = new BinarySearchTreeNode(data);
+          return current.rightNode;
+        }
+      } else {
+        if (current.leftNode) {
+          current = current.leftNode;
+        } else {
+          current.leftNode = new BinarySearchTreeNode(data);
+          return current.leftNode;
+        }
+      }
+    }
+  }
+
+  search(data: T): BinarySearchTreeNode<T> | undefined {
+    if (!this.root) return undefined;
+
+    let current = this.root;
+
+    while (this.comparator(data, current.data) !== 0) {
+      if (this.comparator(data, current.data) === 1) {
+        if (!current.rightNode) return;
+
+        current = current.rightNode;
+      } else {
+        if (!current.leftNode) return;
+
+        current = current.leftNode;
+      }
+    }
+
+    return current;
+  }
+
+  inOrderTraversal(node: BinarySearchTreeNode<T> | undefined): void {
+    if (node) {
+      this.inOrderTraversal(node.leftNode);
+      console.log(node.data);
+      this.inOrderTraversal(node.rightNode);
+    }
+  }
+
+  preOrderTraversal(node: BinarySearchTreeNode<T> | undefined): void {
+    if (node) {
+      console.log(node.data);
+      this.preOrderTraversal(node.leftNode);
+      this.preOrderTraversal(node.rightNode);
+    }
+  }
+
+  postOrderTraversal(node: BinarySearchTreeNode<T> | undefined): void {
+    if (node) {
+      this.postOrderTraversal(node.leftNode);
+      this.postOrderTraversal(node.rightNode);
+      console.log(node.data);
+    }
+  }
+  height(node: BinarySearchTreeNode<T> | undefined) {
+    if (node) {
+      return (
+        1 + Math.max(this.height(node.leftNode), this.height(node.rightNode))
+      );
+    } else {
+      return 0;
+    }
+  }
+}
+
+function comparator(a: number, b: number) {
+  if (a < b) return -1;
+
+  if (a > b) return 1;
+
+  return 0;
+}
+function main() {
+  // Enter your code here
+  const inputLines = [3, 5, 2, 1, 4, 6, 7];
+  let bb = new BinarySearchTree(comparator);
+  for (let i = 0; i < inputLines.length; i++) {
+    bb.insert(inputLines[i]);
+  }
+  bb.preOrderTraversal(bb.root);
+  console.log('=================================');
+  // bb.inOrderTraversal(bb.root);
+  // console.log('================================');
+  console.log(`height of the tree: ${bb.height(bb.root) - 1}`);
+
+  // const bst = new BinarySearchTree(comparator);
+
+  // bst.insert(5);
+
+  // bst.insert(2);
+  // bst.insert(3);
+  // bst.insert(1);
+
+  // bst.insert(7);
+  // bst.insert(6);
+  // bst.insert(8);
+
+  // bst.inOrderTraversal(bst.root);
+}
+main();
